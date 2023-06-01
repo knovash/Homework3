649b7dbd51319115ab5cea1c1bee621826586d74
Konstantin <novash.ki@gmail.com>
2023-06-01 21:37:48 +0300

    bfmethod getpage. bfclass timeouts bftest getdriver

--- src/test/java/org/itacademy/homework3/DeliveryPageTest.java
+++ src/test/java/org/itacademy/homework3/DeliveryPageTest.java
@@ -9,25 +9,30 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.time.Duration;
+
 public class DeliveryPageTest extends BaseTest {
 
     private DeliveryPage deliveryPage;
 
     @BeforeClass
     public void beforeclass() {
-        driver = DriverManager.getDriver();
+//        driver = DriverManager.getDriver(); // достаточно создать в бефо тест
         System.out.println("\nBEFORE CLASS DRIVER (in test) " + driver);
-        driver.get(Config.getPageDelivery());
-        deliveryPage = new DeliveryPage(driver);
+        // для этого класса понижаем ожидание чтоб долго неждать если адрес ненайден
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//        driver.get(Config.getPageDelivery());
+//        deliveryPage = new DeliveryPage(driver);
     }
 
 
     @BeforeMethod
     public void beforemethod() {
-        driver = DriverManager.getDriver();
+//        driver = DriverManager.getDriver(); // достаточно создать в бефо класс
         System.out.println("\nBEFORE METHOD DRIVER (in test) " + driver);
         driver.get(Config.getPageDelivery());
-        deliveryPage = new DeliveryPage(driver);
+        deliveryPage = new DeliveryPage(driver); // обновляет браузер чтоб ввести в пустые поля
+        // перед каждым запуском метода с новым элементом датапровайдера
     }
 
     @DataProvider(name = "addresses", parallel = false) // если нет имени то определяется по имени метода
@@ -51,12 +56,14 @@ public class DeliveryPageTest extends BaseTest {
         }
         System.out.println("STATUS " + status);
         Assert.assertTrue(status, "NOT SUCCESS");
-//        WaitUtils.waitSeconds(5); // подождать посмотреть на результат поиска
+        WaitUtils.waitSeconds(3); // подождать посмотреть на результат поиска
     }
 
     @Test(testName = "NotTest", description = "Verifys nothing", enabled = true)
     public void verifyNothing() {
         System.out.println("\nTEST NOTHING");
+        deliveryPage.getFieldStreet().sendKeys("street");
+        WaitUtils.waitSeconds(3); // подождать посмотреть на результат поиска
         Assert.assertTrue(true, "NOT TRUE");
     }
 }
